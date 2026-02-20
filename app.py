32e0f016035c0b94e4e33945ab28ccda198df9e0
expedicaoserraazul <fiscalsupermercadoserraazul@gmail.com>
2026-02-20 18:29:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
-app.py
-users.py
+import streamlit as st
+
+st.set_page_config(page_title="Portal Expedição", layout="centered")
+
+st.title("Portal Expedição 🚚")
+st.success("Sistema online com sucesso!")
