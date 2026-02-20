da0994dcd7471efd9b266b638264735a3083c83d
expedicaoserraazul <fiscalsupermercadoserraazul@gmail.com>
2026-02-20 19:14:40 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,1 +1,17 @@
-pages/home.py
+import streamlit as st
+
+def render(user):
+    st.title("📦 Portal de Expedição")
+
+    st.success(f"Bem-vindo, {user['name']}")
+
+    st.write("**Cargo:**", user["cargo"])
+    st.write("**Setor:**", user["setor"])
+    st.write("**Perfil:**", user["role"])
+
+    st.divider()
+
+    if st.button("Sair"):
+        st.session_state.authenticated = False
+        st.session_state.user_data = None
+        st.rerun()
