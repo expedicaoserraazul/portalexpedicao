9ee21fc9d74b19dc9d1b6dd3621b2a28240962de
expedicaoserraazul <fiscalsupermercadoserraazul@gmail.com>
2026-03-05 10:10:47 -0300

    Update tarefa_recebimento.py

--- telas/tarefa_recebimento.py
+++ telas/tarefa_recebimento.py
@@ -63,7 +63,7 @@ def tela_tarefa(usuario="prevenção", loja="Loja 01"):
     <style>
 
     .block-container {{
-        padding-bottom:120px;
+        padding-bottom:200px;
     }}
 
     .barra-envio {{
